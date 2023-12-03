d995d2e5f608a4065af73d4a54a5b0c591ef6692
Your Name <you@example.com>
2023-12-03 19:31:30 +0600

    first commit

--- app/Http/Controllers/CollectionController.php
+++ app/Http/Controllers/CollectionController.php
@@ -6,5 +6,29 @@ use Illuminate\Http\Request;
 
 class CollectionController extends Controller
 {
-    //
+    public function index()
+    {
+        
+        $collections = []; 
+        return view('collections.index', ['collections' => $collections]);
+    }
+
+    public function create()
+    {
+        return view('collections.create');
+    }
+
+    public function store(Request $request)
+    {
+       
+
+        return redirect()->route('collections.index');
+    }
+
+    public function edit($id)
+    {
+        
+        $collection = []; 
+        return view('collections.edit', ['collection' => $collection]);
+    }
 }
