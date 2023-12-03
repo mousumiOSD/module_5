326bb1f405b95cf09714992e89e8504be55d94af
Your Name <you@example.com>
2023-12-03 19:53:23 +0600

    first commit

--- app/Http/Controllers/ShopController.php
+++ app/Http/Controllers/ShopController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class ShopController extends Controller
 {
-    //
+        public function index()
+        {
+
+            return view('shop.index', ['shopName' => 'Your Shop Name', 'shopId' => 'Your Shop ID']);
+        }
 }
